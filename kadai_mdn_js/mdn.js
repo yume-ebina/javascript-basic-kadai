45769de6c1320082bb437fab6b266f6578254ed0
yume-ebina <cityeventa@gmail.com>
2023-12-05 10:43:04 +0900

    update kadai_mdn

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,3 +1,6 @@
-let date = new Date(2023, 7, 22);
+let date = new Date(2023, 8, 22);
+let year = date.getFullYear();
+let month = date.getMonth();
+let day = date.getDate();
 
-console.log(date);
+console.log(year + '年' + month + '月' + day + '日');
